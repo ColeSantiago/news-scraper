c52561fc43e01d9bb6c80f0959cca83aad89e147
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-04-01 13:32:08 -0400

    file structure set up, npm packages installed with package.json, server.js set up

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const exphbs = require("express-handlebars");
+
+const db = require("./models");
+
+const PORT = 3000;
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
+const apiRoutes = require("./routes/api-routes.js");
+require("./routes/html-routes.js")(app);
+
+app.use(apiRoutes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
